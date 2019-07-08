89d21adfd083f470380c3f0cb46aaaa71eeeef6e
Tracy Reuther <tracyreuther@gmail.com>
2019-07-08 23:07:08 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,8 +3,12 @@ require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 
 class Kid
-
   extend FancyDance::ClassMethods
-  include FancyDance::InstanceMethods
+  include FancyDance::InstanceMethods 
 
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
 end
